172f65cd3bf2d698d3d201adfda544db01b5dccf
panguss <113355417+panguss@users.noreply.github.com>
2022-12-21 01:29:20 +0500

    Create lab08.py

---
+++ lab08.py
@@ -0,0 +1,24 @@
+import random
+import logging
+
+logging.basicConfig(filename='py.log', level=logging.INFO)
+
+while True:
+    try:
+        a = int(input("Введите число бочонков: "))
+        assert a > 0
+    except AssertionError:
+        print("Число должно быть больше нуля\n Попробуйте еще раз")
+        logging.warning("not correct number")
+    except ValueError:
+        print('Необходимо ввести положительное число \n Попробуйте еще раз')
+        logging.exception('not correct input')
+    else:
+        logging.info('correct number')
+        break
+list = [i for i in range(1, a + 1)]
+while list:
+    num = random.choice(list)
+    while num in list:
+        list.remove(num)
+    print(num, end=" ")
